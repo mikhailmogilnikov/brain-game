e9a679c09909abd9ccc213d7a680de8f55fd8037
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-09-26 17:52:41 +0300

    refactor: brain-calc.js

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -3,27 +3,32 @@ import greeting from '../../src/cli.js';
 import game from '../../src/index.js';
 import { randomNum, randomOperand } from '../../src/serviceFunctions.js';
 
+const calculateResult = (a, b, operand) => {
+  switch (operand) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return null;
+  }
+};
+
+const generateTask = () => {
+  const a = randomNum();
+  const b = randomNum();
+  const operand = randomOperand();
+  const task = `${a} ${operand} ${b}`;
+  const correctAnswer = calculateResult(a, b, operand);
+  return { task, correctAnswer };
+};
+
 const gameCalc = (user) => {
   const maxAttempts = 3;
-  let correctAnswer;
   for (let i = 0; i < maxAttempts; i += 1) {
-    const a = randomNum();
-    const b = randomNum();
-    const operand = randomOperand();
-    const task = `${a} ${operand} ${b}`;
-    switch (operand) {
-      case '+':
-        correctAnswer = a + b;
-        break;
-      case '-':
-        correctAnswer = a - b;
-        break;
-      case '*':
-        correctAnswer = a * b;
-        break;
-      default:
-        return null;
-    }
+    const { task, correctAnswer } = generateTask();
     const result = game(task, correctAnswer);
     if (!result) {
       return null;
